bb491f1445dea8c7c37d0b548964900824f9715a
shinji0320 <shinji.blue.one@gmail.com>
2021-03-26 08:39:23 +0900

    Questionの内容表示

---
+++ src/components/Chat.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+
+
+const Chat = (props) => {
+  const isQuestion = (props.type === 'question');
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+  return(
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        <Avatar alt="icon" src="/static/images/avatar/2.jpg" />
+      </ListItemAvatar>
+      <div className="p-chat__bubble">{props.text}</div>
+    </ListItem>
+  )
+}
+
+export default Chat
